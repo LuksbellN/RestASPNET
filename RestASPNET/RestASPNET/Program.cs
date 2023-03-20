3d42fe10bc96b8f74ef4b5cb68288a2a6290b9b9
Luksbell <belchior1100@gmail.com>
2023-03-20 14:21:58 -0300

    Implementing GET,POST,DELETE methods with Mock Data

--- RestASPNET/RestASPNET/Program.cs
+++ RestASPNET/RestASPNET/Program.cs
@@ -1,7 +1,11 @@
+using RestASPNET.Services;
+using RestASPNET.Services.Implementations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ RestASPNET/RestASPNET/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,65 @@
+﻿using RestASPNET.Model;
+
+namespace RestASPNET.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = id,
+                FirstName = "Lucas",
+                LastName = "Costa",
+                Address = "Uberlandia - Minas Gerais - Brasil",
+                Gender = "Male"
+            };
+        }
+
+        public List<Person> GetAll()
+        {
+            List<Person> persons = new List<Person>();
+            for(int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+
+        public Person Update(Person person)
+        {
+
+            return person;
+        }
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name" + i,
+                LastName = "Person Last Name"+i,
+                Address = "Some Address" + i,
+                Gender = "Random"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
